312cb084889bf786bc7556c7534ed86e3248c387
JulianoQueiroz <juliano.invest@outlook.com>
2021-06-17 17:13:32 -0300

    Árvore binaria - Expecificando a raiz

---
+++ README.md
@@ -0,0 +1,14 @@
+# Estrutura da árvore binária 
+
+        '+'
+      /     \ 
+    'a'    '*'
+          /   \
+        'b'   '-'
+             /   \
+           '/'  'e'
+           / \      
+         'c' 'd'  
+
+# Expressão aritimética binaria
+(a + ((b * (c/d) - e)))
--- tree.py
+++ tree.py
@@ -1,7 +1,7 @@
 
 
 class Node:
-    def __init__(self, data) -> None:
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
@@ -10,10 +10,54 @@ class Node:
         return str(self.data)
         
 class BinaryTree:
-    def __init__(self, data) -> None:
-        node = Node(data)
-        self.root = node
-        
+    def __init__(self, data=None):
+        if data:
+            node = Node(data)
+            self.root = node
+        else:
+            self.root = None
+    
+    # percurso de ordem simetrica
+    def simetric_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            print('(', end='')
+            self.simetric_traversal(node.left)
+        print(node, end='')
+        if node.right:
+            self.simetric_traversal(node.right)
+            print(')', end='')
+          
 if __name__ == "__main__":
-    tree = BinaryTree(7)
-    tree.root.left = Node(18)
+    # tree = BinaryTree(7)
+    # tree.root.left = Node(18)
+    # tree.root.right = Node(14)
+    
+    # print(tree.root)
+    # print(tree.root.right)
+    # print(tree.root.left)
+    tree = BinaryTree()
+    n1 = Node('a')
+    n2 = Node('+')
+    n3 = Node('*')
+    n4 = Node('b')
+    n5 = Node('-')
+    n6 = Node('/')
+    n7 = Node('c')
+    n8 = Node('d')
+    n9 = Node('e')
+    
+    n6.left = n7
+    n6.right = n8
+    n5.left = n6
+    n5.right = n9
+    n3.left = n4
+    n3.right = n5
+    n2.left = n1
+    n2.right = n3
+    
+    tree.root = n2
+    
+    tree.simetric_traversal()
+    print()
